eaaf1662aa023b67cb5e348e262936a1d109ae7a
Marcos Junior <m.juniorcsebastiao@gmail.com>
2019-07-07 03:54:10 -0300

    fix: forgot to commit the TODO comment

--- src/checker.hpp
+++ src/checker.hpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// TODO: add other operators, project is way too simple as it is now
+
 bool isOperator(std::string token) {
     return (token == "+" || token == "-" || token == "*" || token == "/");
 }
